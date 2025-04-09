201410b31d3552cf5094dac1bd1833ca62ce679a
Faisal Irfan <faisalirfan619@gmail.com>
2025-04-09 22:01:33 +0500

    reports

---
+++ src/app/dashboard/reports/page.jsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function ReportsPage() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-3xl font-bold">Feature Coming Soon</h1>
+    </div>
+  );
+}
